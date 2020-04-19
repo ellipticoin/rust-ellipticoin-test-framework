5f3407ad4e02056631a27e6f0ac20802d5dd681d
Mason Fischer <mason@kissr.co>
2020-04-19 16:56:30 -0400

    Add hash_onion herlpers

--- src/lib.rs
+++ src/lib.rs
@@ -1,29 +1,47 @@
 extern crate hex;
 extern crate rand;
 extern crate secp256k1;
+extern crate sha2;
 
-use self::rand::{thread_rng, Rng};
+use sha2::{Sha256, Digest};
 use self::secp256k1::Message;
+use rand::{rngs::OsRng, Rng};
 mod constants;
 pub use constants::*;
 
+pub fn sha256(value: Vec<u8>) -> Vec<u8> {
+    let mut hasher = Sha256::new();
+    hasher.input(value);
+    hasher.result().to_vec()
+}
+
 pub fn random_bytes(length: usize) -> Vec<u8> {
-    rand::thread_rng().gen_iter::<u8>().take(length).collect()
+    rand::thread_rng()
+        .sample_iter(&rand::distributions::Standard)
+        .take(length)
+        .collect()
+}
+
+pub fn generate_hash_onion(layers: usize, center: Vec<u8>) -> Vec<Vec<u8>> {
+    let mut onion = vec![center];
+    for _ in 1..(layers) {
+        onion.push(sha256(onion.last().unwrap().to_vec()));
+    }
+    onion
 }
 
 pub fn generate_keypair() -> (Vec<u8>, Vec<u8>) {
     let signer = secp256k1::Secp256k1::new();
-    let (private_key, public_key) = signer.generate_keypair(&mut thread_rng());
+    let mut rng = OsRng::new().unwrap();
+    let (private_key, public_key) = signer.generate_keypair(&mut rng);
 
     (private_key[..].to_vec(), public_key.serialize().to_vec())
 }
 
-pub fn secp256k1_sign_recoverable(message_vec: Vec<u8>, private_key_vec: Vec<u8>) -> (u8, Vec<u8>) {
+pub fn secp256k1_sign_recoverable(message_vec: Vec<u8>, private_key_vec: Vec<u8>) -> Vec<u8> {
     let signer = secp256k1::Secp256k1::new();
     let message = Message::from_slice(&message_vec).unwrap();
     let private_key = secp256k1::SecretKey::from_slice(&private_key_vec).unwrap();
-    let (recovery_id, signature) = signer
-        .sign_recoverable(&message, &private_key)
-        .serialize_compact();
-    (recovery_id.to_i32() as u8, signature.to_vec())
+    let signature = signer.sign(&message, &private_key).serialize_compact();
+    signature.to_vec()
 }
